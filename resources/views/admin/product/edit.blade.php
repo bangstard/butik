4f57ed6229e766bf269f7135d0b55c726c80a4a5
Qayyumcheri <muyyaq90@yahoo.com>
2021-11-09 16:37:29 +0800

    change property of html element from placeholder to value to fetch old data

--- resources/views/admin/product/edit.blade.php
+++ resources/views/admin/product/edit.blade.php
@@ -8,30 +8,31 @@
                 <div class="card-header">{{ __('Edit Products') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('product:edit', $product) }}">
+                    <form method="POST" action="{{ route('product:update', $product) }}" enctype="multipart/form-data">
                         @csrf
 
                         <div class="md-3">
                             <label for="name" class="form-label">Name</label>
-                            <input type="text" name="name" class="form-control" id="name" placeholder="{{ $product-> name }}">
+                            <input type="text" name="name" class="form-control" id="name" value="{{ $product-> name }}">
                         </div>
                         <div class="md-3">
                             <label for="price" class="form-label">Price</label>
-                            <input type="text" name="price" class="form-control" id="price" placeholder="{{ $product-> price }}">
+                            <input type="text" name="price" class="form-control" id="price" value="{{ $product-> price }}">
                         </div>
                         <div class="md-3">
                             <label for="name" class="form-label">Description</label>
-                            <input type="text" name="description" class="form-control" id="description" placeholder="{{ $product-> description }}">
+                            <input type="text" name="description" class="form-control" id="description" value="{{ $product-> description }}">
                         </div>
                         <div class="md-3">
                             <label for="image" class="form-label">Image</label>
                             <div>
-                                <input type="file" name="image" class="" id="image" placeholder="{{ $product->image }}">
+                                <input type="file" name="image" class="" id="image" value="{{ $product->image }}">
                             </div>
                         </div>
-
-
-                        <input type="submit" class="form-control" placeholder="">
+                        <br>
+                        <div class="mb-3">
+                            <input type="submit" class="btn btn-success" placeholder="">
+                        </div>
                     </form>
                 </div>
             </div>
