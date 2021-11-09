a2dccf26de38817d88283e03a65a8ac31b92c0f1
Qayyumcheri <muyyaq90@yahoo.com>
2021-11-09 16:34:34 +0800

    add category relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,16 +2,40 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
     protected $fillable = [
+        'user_id',
+        'category_id',
         'name',
         'price',
         'description',
         'image',
     ];
+    
+    /**
+     * Get the user that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get the category that owns the Product
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id');
+    }
 }
