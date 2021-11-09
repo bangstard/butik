2323b01ee611dfc9c3dfb8685cf3b4c3748b26f1
Qayyumcheri <muyyaq90@yahoo.com>
2021-11-09 16:34:11 +0800

    add product relation

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Category extends Model
 {
@@ -12,4 +13,14 @@ class Category extends Model
     protected $fillable = [
         'type',
     ];
+    
+    /**
+     * Get the product associated with the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function product()
+    {
+        return $this->hasOne(Product::class);
+    }
 }
